0f31477f298f6908bb243187354a4768ececf192
Akshat Bawa <akshatbawa130806@gmail.com>
2025-07-09 17:43:31 +0530

    Update ProfilePhotoSelector.jsx

--- frontend/src/components/Inputs/ProfilePhotoSelector.jsx
+++ frontend/src/components/Inputs/ProfilePhotoSelector.jsx
@@ -29,7 +29,7 @@ const ProfilePhotoSelector = ({ image, setImage }) => {
       <div className="w-20 h-20 flex items-center justify-center bg-purple-100 rounded-full relative">
         <LuUser className="text-4xl text-primary" />
         <button 
-          type="button" className="w-8 h-8 flex items-center justify-center bg-primary text-white rounded-full absolute -bottom-1 -right-1" onClick={onChooseFile} 
+          type="button" className="w-8 h-8 flex items-center justify-center bg-primary text-white rounded-full absolute -bottom-1 -right-1 cursor-pointer" onClick={onChooseFile} 
         >
           <LuUpload />
         </button>
