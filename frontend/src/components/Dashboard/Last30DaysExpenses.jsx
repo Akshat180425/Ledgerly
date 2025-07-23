3137bea223bd87fc202d70cd040fe522ae8eef6a
Akshat Bawa <akshatbawa130806@gmail.com>
2025-07-23 19:23:29 +0530

    Update Last30DaysExpenses.jsx

--- frontend/src/components/Dashboard/Last30DaysExpenses.jsx
+++ frontend/src/components/Dashboard/Last30DaysExpenses.jsx
@@ -1,25 +1,23 @@
-import React from 'react' 
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import CustomBarChart from '../Charts/CustomBarChart';
 import { prepareExpenseBarChartData } from '../../utils/helper';
 
-const Last30DaysExpenses = ({data}) => { 
-  const [chartData, setChartData] = useState([]); 
-  useEffect(() => { 
-    const result = prepareExpenseBarChartData(data); 
-    setChartData(result); return () => {}; 
-  }, [data]); 
-  
-  return ( 
-    <div className="card col-span-1"> 
-      <div className="flex items-center justify-between"> 
-        <h5 className="text-1g">Last 30 Days Expenses</h5> 
-      </div> 
-      <CustomBarChart 
-        data={chartData}
-      />
-    </div> 
-  ) 
-}
+const Last30DaysExpenses = ({ data }) => {
+  const [chartData, setChartData] = useState([]);
 
-export default Last30DaysExpenses
+  useEffect(() => {
+    const result = prepareExpenseBarChartData(data);
+    setChartData(result);
+  }, [data]);
+
+  return (
+    <div className="card col-span-1">
+      <div className="flex items-center justify-between">
+        <h5 className="text-lg">Last 30 Days Expenses</h5>
+      </div>
+      <CustomBarChart data={chartData} />
+    </div>
+  );
+};
+
+export default Last30DaysExpenses;
