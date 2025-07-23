165da5309b629379df599e66a7a60bb98335b20f
Akshat Bawa <akshatbawa130806@gmail.com>
2025-07-23 19:38:59 +0530

    Update RecentIncome.jsx

--- frontend/src/components/Dashboard/RecentIncome.jsx
+++ frontend/src/components/Dashboard/RecentIncome.jsx
@@ -6,19 +6,28 @@ import moment from "moment";
 const RecentIncome = ({ transactions, onSeeMore }) => { 
   return ( 
     <div className="card"> 
-      <div className="flex items-center justify-between "> 
-        <h5 className="text-1g">
-          Income
-        </h5> 
-        <button className="card-btn" onClick={onSeeMore}> See All <LuArrowRight className="text-base" /> </button> 
+      <div className="flex items-center justify-between"> 
+        <h5 className="text-lg">Income</h5> 
+        <button className="card-btn" onClick={onSeeMore}> 
+          See All 
+          <LuArrowRight className="text-base" /> 
+        </button> 
       </div> 
       <div className="mt-6"> 
-        {transactions?.slice(0, 5).map( (item) => ( 
-          <TransactionInfoCard key={item._id} title={item.source} icon={item.icon} date={moment(item.date).format("Do MMM YYYY")} amount={item.amount} type="income" hideDeleteBtn /> 
+        {transactions?.slice(0, 5).map((item) => ( 
+          <TransactionInfoCard 
+            key={item._id} 
+            title={item.source} 
+            icon={item.icon} 
+            date={moment(item.date).format("Do MMM YYYY")} 
+            amount={item.amount} 
+            type="income" 
+            hideDeleteBtn 
+          /> 
         ))} 
       </div> 
     </div> 
-  ) 
+  ); 
 };
 
 export default RecentIncome;
