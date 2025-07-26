6604641645850733b75f9c4d8e25bb34218e3363
Akshat Bawa <akshatbawa130806@gmail.com>
2025-07-26 22:19:17 +0530

    Update apiPaths.js

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,30 +1,27 @@
 const baseURL = import.meta.env.VITE_API_URL;
 
-fetch(`${baseURL}/api/expenses`)
-  .then(res => res.json())
-  .then(data => console.log(data));
-
-export const API_PATHS = { 
-  AUTH: { 
-    LOGIN: "/api/v1/auth/login", 
-    REGISTER: "/api/v1/auth/register", 
-    GET_USER_INFO: "/api/v1/auth/getUser", 
-  }, 
-  DASHBOARD: { 
-    GET_DATA: "/api/v1/dashboard", 
-  }, 
-  INCOME: { 
-    ADD_INCOME: "/api/v1/income/add", 
-    GET_ALL_INCOME: "/api/v1/income/get", 
-    DELETE_INCOME: (incomeId) => `/api/v1/income/${incomeId}`, 
-    DOWNLOAD_INCOME: `/api/v1/income/downloadexcel`, 
-  }, 
-  EXPENSE: { 
-    ADD_EXPENSE: "/api/v1/expense/add", 
-    GET_ALL_EXPENSE: "/api/v1/expense/get", 
-    DELETE_EXPENSE: (expenseId) => `/api/v1/expense/${expenseId}`, 
-    DOWNLOAD_EXPENSE: `/api/v1/expense/downloadexcel`, 
-  }, IMAGE: { 
-    UPLOAD_IMAGE: "/api/v1/auth/upload-image", 
-  }, 
+export const API_PATHS = {
+  AUTH: {
+    LOGIN: `${baseURL}/api/v1/auth/login`,
+    REGISTER: `${baseURL}/api/v1/auth/register`,
+    GET_USER_INFO: `${baseURL}/api/v1/auth/getUser`,
+  },
+  DASHBOARD: {
+    GET_DATA: `${baseURL}/api/v1/dashboard`,
+  },
+  INCOME: {
+    ADD_INCOME: `${baseURL}/api/v1/income/add`,
+    GET_ALL_INCOME: `${baseURL}/api/v1/income/get`,
+    DELETE_INCOME: (incomeId) => `${baseURL}/api/v1/income/${incomeId}`,
+    DOWNLOAD_INCOME: `${baseURL}/api/v1/income/downloadexcel`,
+  },
+  EXPENSE: {
+    ADD_EXPENSE: `${baseURL}/api/v1/expense/add`,
+    GET_ALL_EXPENSE: `${baseURL}/api/v1/expense/get`,
+    DELETE_EXPENSE: (expenseId) => `${baseURL}/api/v1/expense/${expenseId}`,
+    DOWNLOAD_EXPENSE: `${baseURL}/api/v1/expense/downloadexcel`,
+  },
+  IMAGE: {
+    UPLOAD_IMAGE: `${baseURL}/api/v1/auth/upload-image`,
+  },
 };
