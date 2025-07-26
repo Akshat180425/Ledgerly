24e50dce050dea03814eca0b180f981f708f3079
Akshat Bawa <akshatbawa130806@gmail.com>
2025-07-26 17:44:58 +0530

    Update CustomLineChart.jsx

--- frontend/src/components/Charts/CustomLineChart.jsx
+++ frontend/src/components/Charts/CustomLineChart.jsx
@@ -7,7 +7,7 @@ const CustomLineChart = ({ data }) => {
         <div className="bg-white shadow-md rounded-lg p-2 border border-gray-300"> 
           <p className="text-xs text-semibold text-purple-800 mb-1">{payload[0].payload.category}</p> 
           <p className="text-sm text-gray-600"> 
-            Amount: <span className="text-sm font-medium text-gray-900">${payload[0].payload.amount} </span>
+            Amount: <span className="text-sm font-medium text-gray-900">₹{payload[0].payload.amount} </span>
           </p> 
         </div> 
       ); 
@@ -27,7 +27,7 @@ const CustomLineChart = ({ data }) => {
           <CartesianGrid stroke="none" />
           <XAxis
             dataKey="month"
-            tick={{ fontSize: 12, fill: "#555" }}
+            tick={{ fontSize: 12, fill: "#555" }} 
             axisLine={false}
             tickLine={false}
           />
