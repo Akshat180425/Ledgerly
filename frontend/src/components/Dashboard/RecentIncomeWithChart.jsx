524309878193e0b4e0083f4c6794d12fe943bdb2
Akshat Bawa <akshatbawa130806@gmail.com>
2025-07-27 13:15:19 +0530

    Update RecentIncomeWithChart.jsx

--- frontend/src/components/Dashboard/RecentIncomeWithChart.jsx
+++ frontend/src/components/Dashboard/RecentIncomeWithChart.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import CustomLineChart from '../Charts/CustomLineChart';
+import CustomLineChart from '../Charts/CustomLineChartForIncomes';
 import { prepareIncomeBarChartData } from '../../utils/helper';
 
 const RecentIncomeWithChart = ({ data, totalIncome }) => {
