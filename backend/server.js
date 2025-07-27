5b13b4269e30229230403c47cb018dcc31185b18
Akshat Bawa <akshatbawa130806@gmail.com>
2025-07-27 15:14:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://ledgerly-09sf.onrender.com",
+      "https://ledgerly-five.vercel.app/",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
