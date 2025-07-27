7c63641087bf0883e37680c160a480b5c4e589ae
Akshat Bawa <akshatbawa130806@gmail.com>
2025-07-27 13:51:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://ledgerly-vjvk.onrender.com",
+      "https://ledgerly-09sf.onrender.com",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
